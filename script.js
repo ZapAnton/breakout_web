e9f15e6692b1102623088400bc01798ab196bbd3
ZapAnton <wc3ft2@gmail.com>
2024-11-04 21:20:51 +0200

    Added bricks field drawing

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ const PADDLE_WIDTH = 75;
 const PADDLE_HEIGHT = 10;
 const PADDLE_COLOR = "#0095DD";
 const BALL_COLOR = "#0095DD";
+const BRICK_COLOR = "#0095DD";
 const BALL_RADIUS = 10;
+const COLUMN_COUNT = 10;
+const ROW_COUNT = 5;
+const BRICK_WIDTH = 75;
+const BRICK_HEIGHT = 20;
+const BRICK_PADDING = 10;
+const BRICK_OFFSET_TOP = 30;
+const BRICK_OFFSET_LEFT = 30;
+
 const canvas = document.getElementById("gameCanvas");
 const context = canvas.getContext("2d");
 canvas.width = FIELD_WIDTH;
@@ -12,6 +21,13 @@ canvas.height = FIELD_HEIGHT;
 const paddleX = (canvas.width - PADDLE_WIDTH) / 2;
 const ballX = canvas.width / 2;
 const ballY = canvas.height - 30;
+const bricks = [];
+for (let col = 0; col < COLUMN_COUNT; ++col) {
+    bricks[col] = [];
+    for (let row = 0; row < ROW_COUNT; ++row) {
+        bricks[col][row] = {x: 0, y: 0, isBroken: false}
+    }
+}
 
 function drawPaddle() {
     context.beginPath();
@@ -29,9 +45,29 @@ function drawBall() {
     context.closePath();
 }
 
+function drawBricks() {
+    for (let col = 0; col < COLUMN_COUNT; ++col) {
+        for (let row = 0; row < ROW_COUNT; ++row) {
+            if (bricks[col][row].isBroken) {
+                continue;
+            }
+            const brickX = col * (BRICK_WIDTH + BRICK_PADDING) + BRICK_OFFSET_LEFT;
+            const brickY = row * (BRICK_HEIGHT + BRICK_PADDING) + BRICK_OFFSET_TOP;
+            bricks[col][row].x = brickX;
+            bricks[col][row].y = brickY;
+            context.beginPath();
+            context.rect(brickX, brickY, BRICK_WIDTH, BRICK_HEIGHT);
+            context.fillStyle = BRICK_COLOR;
+            context.fill();
+            context.closePath();
+        }
+    }
+}
+
 function main() {
     drawBall();
     drawPaddle();
+    drawBricks();
 }
 
 main();
