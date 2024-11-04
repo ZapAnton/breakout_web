6635d12cfcdda5d533da9d22ce8e900746e07b42
ZapAnton <wc3ft2@gmail.com>
2024-11-04 18:38:22 +0200

    Added game paddle drawing

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const FIELD_WIDTH = 900;
 const FIELD_HEIGHT = 600;
+const PADDLE_WIDTH = 75;
+const PADDLE_HEIGHT = 10;
+const PADDLE_COLOR = "#0095DD";
 const canvas = document.getElementById("gameCanvas");
 const context = canvas.getContext("2d");
 canvas.width = FIELD_WIDTH;
 canvas.height = FIELD_HEIGHT;
+const paddleX = (canvas.width - PADDLE_WIDTH) / 2;
+
+function drawPaddle() {
+    context.beginPath();
+    context.rect(paddleX, canvas.height - PADDLE_HEIGHT, PADDLE_WIDTH, PADDLE_HEIGHT);
+    context.fillStyle = PADDLE_COLOR;
+    context.fill();
+    context.closePath();
+}
+
+function main() {
+    drawPaddle();
+}
+
+main();
