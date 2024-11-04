c1afab153b8f9fc772c72fd5f242e46433aabd30
ZapAnton <wc3ft2@gmail.com>
2024-11-04 18:31:06 +0200

    Added basic game field settings via script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const FIELD_WIDTH = 900;
+const FIELD_HEIGHT = 600;
+const canvas = document.getElementById("gameCanvas");
+const context = canvas.getContext("2d");
+canvas.width = FIELD_WIDTH;
+canvas.height = FIELD_HEIGHT;
